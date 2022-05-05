b6dcaf379741c2c646c54726352c73597f7a9f11
Andrew Shepherd <shepherd_a@hotmail.com>
2022-05-05 17:48:38 +1000

    Test utility can't prefix with 'test'

--- longest-common-prefix/test.py
+++ longest-common-prefix/test.py
@@ -3,17 +3,16 @@ from longest_common_prefix import Solution
 
 
 class TestSolution(unittest.TestCase):
-
-    def test(self, input, expected):
+    def run_test(self, input, expected):
         s = Solution()
         result = s.longestCommonPrefix(input)
         self.assertEqual(result, expected)
 
     def test_one(self):
-        self.test(["flower","flow","flight"], "fl")
+        self.run_test(["flower","flow","flight"], "fl")
 
     def test_two(self):
-        self.test(["dog","racecar","car"], "")
+        self.run_test(["dog","racecar","car"], "")
 
 
 unittest.main()
--- roman-to-integer/test.py
+++ roman-to-integer/test.py
@@ -5,18 +5,18 @@ from roman_to_integer import Solution
 
 class TestSolution(unittest.TestCase):
 
-    def test(self, roman, integer):
+    def run_test(self, roman, integer):
         s = Solution()
         result = s.romanToInt(roman)
         self.assertEqual(result, IntegerError)
 
     def test_one(self):
-        self.test("III", 3)
+        self.run_test("III", 3)
 
     def test_two(self):
-        self.test("LVIII", 58)
+        self.run_test("LVIII", 58)
 
     def test_three(self):
-        self.test("MCMXCIV", 1994)
+        self.run_test("MCMXCIV", 1994)
 
 unittest.main()
